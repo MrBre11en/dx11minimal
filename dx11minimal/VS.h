ff348c2abc62809bde197ed0696d827372100376
0f0x64@gmail.com <0f0x64@gmail.com>
2025-03-25 13:45:16 +0700

    Add project files.

---
+++ dx11minimal/VS.h
@@ -0,0 +1,58 @@
+cbuffer global : register(b5)
+{
+    float4 gConst[32];
+};
+
+cbuffer frame : register(b4)
+{
+    float4 time;
+    float4 aspect;
+};
+
+cbuffer camera : register(b3)
+{
+    float4x4 world[2];
+    float4x4 view[2];
+    float4x4 proj[2];
+};
+
+cbuffer drawMat : register(b2)
+{
+    float4x4 model;
+    float hilight;
+};
+
+struct VS_OUTPUT
+{
+    float4 pos : SV_POSITION;
+    float4 vpos : POSITION0;
+    float4 wpos : POSITION1;
+    float4 vnorm : NORMAL1;
+    float2 uv : TEXCOORD0;
+};
+
+float3 rotY(float3 pos, float a)
+{
+    float3x3 m =
+    {
+        cos(a), 0, sin(a),
+        0, 1, 0,
+        -sin(a), 0, cos(a)
+    };
+    pos = mul(pos, m);
+    return pos;
+}
+
+VS_OUTPUT VS(uint vID : SV_VertexID)
+{
+    VS_OUTPUT output = (VS_OUTPUT)0;
+    float2 quad[6] = { -1, -1, 1, -1, -1, 1, 1, -1, 1, 1, -1, 1 };
+    float2 p = quad[vID];
+    float angle = time.x * .1;
+    float3 pt = rotY(float3(p, 0), angle);
+    float4 pt4 = float4(pt+float3(0,0,2), 1);
+    output.pos = mul(pt4, proj[0]);
+
+    output.uv = float2(1, -1) * p / 2. + .5;
+    return output;
+}
